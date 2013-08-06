7b60097f7c0bb185accdd55edc7c44972b0ff865
Fabian Aguirre <fabianraf@gmail.com>
2013-08-05 21:06:24 -0500

    First commit

---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+
+require 'bundler/capistrano'
+set :application, "foodcon"
+load "lib/deployer/deployer.rb"
+global_defaults 
+
+set :repository,  "git@github.com:fabianraf/foodcon.git"
+set :scm, :git
+set :user, "deployer"  # The server's user for deploys
+ssh_options[:forward_agent] = true
+set :branch, "master"
+set :git_enable_submodules, 1
+
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+task :preview do
+  set :rails_env, "preview"
+  set :ruby_path, "/usr/local/ruby/bin"
+  server "50.116.60.170", :app, :web, :db, :primary => true
+  defaults
+  set :default_environment, {'PATH'=> "/usr/local/ruby/bin:$PATH"}
+  set :deploy_via, :remote_cache  
+  set :apache_user, "www-data"
+  set :apache_group, "dev"
+  set :apache_dir, "/etc/apache2"
+  set :apache_logs, "/var/log/apache2/#{application}-#{rails_env}"  
+  set :apache_config, "/etc/apache2/sites-available"
+end
+
+task :production do
+  set :rails_env, "production"
+  set :ruby_path, "/usr/local/ruby/bin"
+  server "50.116.60.170", :app, :web, :db, :primary => true
+  defaults
+  set :default_environment, {'PATH'=> "/usr/local/ruby/bin:$PATH"}
+  set :deploy_via, :remote_cache  
+  set :apache_user, "www-data"
+  set :apache_group, "dev"
+  set :apache_dir, "/etc/apache2"
+  set :apache_logs, "/var/log/apache2/#{application}-#{rails_env}"  
+  set :apache_config, "/etc/apache2/sites-available"
+end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodconNuevo::Application.config.secret_token = 'e5427549adaf9cb06a094d109dc572dfcadbb4de4831ed3c3041c053674d80c7ecc8aa2b609ec68a0f91b6a686819fd299b698c884156ab009041d677891c9d4'
